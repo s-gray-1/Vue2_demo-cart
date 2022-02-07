8dcc10a481e6e911c49e67ae019baa9206891443
s-gray-1 <s_gray@foxmail.com>
2022-02-07 15:42:28 +0800

    Vue2购物车案例

---
+++ src/components/Counter/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="number-container d-flex justify-content-center align-items-center">
+    <!-- 减 1 的按钮 -->
+    <button type="button" class="btn btn-light btn-sm" @click="sub">-</button>
+    <!-- 购买的数量 -->
+    <span class="number-box">{{ num }}</span>
+    <!-- 加 1 的按钮 -->
+    <button type="button" class="btn btn-light btn-sm" @click="add">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    num:{
+      type:Number,
+      default:1
+    }
+  },
+  methods:{
+    add(){
+      // 通过自定义事件的方法把 最新的数量值传递给父组件
+      this.$emit('num-change',this.num+1)
+    },
+    sub(){
+      if(this.num==0) return
+      // 通过自定义事件的方法把 最新的数量值传递给父组件
+      this.$emit('num-change',this.num-1)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.number-box {
+  min-width: 30px;
+  text-align: center;
+  margin: 0 5px;
+  font-size: 12px;
+}
+
+.btn-sm {
+  width: 30px;
+}
+</style>
